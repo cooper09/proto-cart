f3f80408d6cfe002d868b2085f2d278def02fbb9
cooper09 <cooper@sonyainc.net>
2020-05-05 13:37:57 -0400

    single box version

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -8,7 +8,7 @@
         <v-responsive class="pt-4">
             
  <!-- <center><img src="https://via.placeholder.com/300" /></center> -->
-            <center><v-img src="http://sonyainc.net/images/single-box.jpg" aspect-ratio="1.7"  id="bgImg"></v-img></center>
+            <center><v-img src="http://sonyainc.net/images/single-box.jpg" aspect-ratio="1."  id="bgImg"></v-img></center>
         
         </v-responsive>
         <v-card-text>
